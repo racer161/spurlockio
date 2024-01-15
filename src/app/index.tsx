d0147995f6c2ea3fb55405f5712c12bcf7349cba
racer161 <ethan@spurlock.io>
2024-01-15 00:24:43 -0600

    First Post Render Test

---
+++ src/app/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Obsidian Next</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Obsidian Next</h1>
+        <p>Obsidian Next is a Next.js starter template for Obsidian.md.</p>
+        <p>It allows you to build a website from your Obsidian vault.</p>
+        <p>It uses the</p>
+      </main>
+    </div>
+  );
+}
---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,58 @@
+import { recFindByExt } from '../../util'
+import markdownit from 'markdown-it'
+import fs from 'fs'
+
+const md = markdownit('commonmark');
+const package_root_dir = process.cwd();
+const vault_path = package_root_dir + '/vault/SPURLOCKIO';
+
+//Root post renderer
+//Loops through all obsidian markdown notes and renders them as posts
+
+export default function Post({ post }: { post: string }) {
+
+  const content = md.render(post);
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: content }} />
+  )
+}
+
+export async function getStaticPaths() {
+
+  const paths = recFindByExt(vault_path, 'md').map((file: string) => {
+
+    const file_full_path = file;
+
+    const trimmed_file_path = file_full_path.substring(vault_path.length);
+
+    console.log(trimmed_file_path);
+
+    return {
+      params: {
+        id: trimmed_file_path
+      },
+    }
+  });
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }: { params: { id: string, file_path: string, path: string } }) {
+
+  console.log(params);
+
+  const file_name = params.id.split('/').pop() ?? params.id;
+
+  const trimmed_file_name = file_name.toLocaleLowerCase().replace(/\.md$/, '').replace(' ', '-');
+
+  //read file contents to string
+  const post = fs.readFileSync(vault_path + params.id, 'utf8');
+
+  // Pass post data to the page via props
+  return { props: { post, trimmed_file_name } }
+}
